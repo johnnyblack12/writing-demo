1cca40afde431039555d045de5b3df6fbcc307a2
John Febbo <jfebbo5@gmail.com>
2022-01-26 21:41:41 -0500

    Update algo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const xValues = [2015,2016,2017,2018,2019,2020,2021,2022];
+const xValues = [0,'Fight 1','Fight 2','Fight 3','Fight 4','Fight 5','Fight 6','Fight 7','Fight 8','Fight 9','Fight 10','Fight 11','Fight 12','Fight 13'];
 
 new Chart('chart1', {
     type: 'line',
@@ -6,12 +6,12 @@ new Chart('chart1', {
         labels: xValues,
         datasets: [{
             label: 'Ngannou',
-            data: [0,0.087,0.387,1.567,1.934,2.539,2.734,4.587],
+            data: [0,1.474,2.808,4.703,6.133,8.743,12.516,11.916,10.249,12.712,13.178,14.643,15.491,19.021],
             borderColor:'red',
             fill: false,
         }, {
             label: 'Gane',
-            data: [0,0,0,0,0,0.239,0.426,1.248],
+            data: [0,1.72,2.736,4.069,7.140,8.473,9.051,10.961],
             borderColor:'blue',
             fill: false,
         }]
@@ -27,12 +27,12 @@ new Chart('chart2', {
         labels: xValues,
         datasets: [{
             label: 'Ngannou',
-            data: [0,0.087,0.387,1.567,1.934,2.539,2.734,4.587],
+            data: [0,1.474,2.808,4.703,6.133,8.743,12.516,11.916,10.249,12.712,13.178,14.643,15.491,19.021],
             borderColor:'red',
             fill: false,
         }, {
             label: 'Gane',
-            data: [0,0.239,0.426,1.248,1.516,1.783,2.051,2.318],
+            data: [0,1.72,2.736,4.069,6.873,8.473,9.051,10.961,12.527,14.093,15.659,17.225,18.79,20.356],
             borderColor:'blue',
             fill: false,
         }]
