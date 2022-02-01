6d49abbdfe7141e0fa9d287c129997344e81160b
John Febbo <jfebbo5@gmail.com>
2022-01-31 19:22:40 -0500

    Update charts

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-const xValues = [0,'Fight 1','Fight 2','Fight 3','Fight 4','Fight 5','Fight 6','Fight 7','Fight 8','Fight 9','Fight 10','Fight 11','Fight 12','Fight 13'];
+const xValues1 = [0,'Fight 1','Fight 2','Fight 3','Fight 4','Fight 5','Fight 6','Fight 7','Fight 8','Fight 9','Fight 10','Fight 11','Fight 12','Fight 13'];
+const xValues2 = [0,'Fight 1','Fight 2','Fight 3','Fight 4','Fight 5'];
 
 new Chart('chart1', {
     type: 'line',
     data: {
-        labels: xValues,
+        labels: xValues1,
         datasets: [{
             label: 'Ngannou',
-            data: [0,1.474,2.808,4.703,6.133,8.743,12.516,11.916,10.249,12.712,13.178,14.643,15.491,19.021],
+            data: [0,1.474,2.808,4.703,6.133,8.743,13.459,12.859,11.193,13.656,14.122,15.586,16.434,19.965],
             borderColor:'red',
             fill: false,
         }, {
             label: 'Gane',
-            data: [0,1.72,2.736,4.069,7.140,8.473,9.051,10.961],
+            data: [0,1.72,2.736,4.069,6.873,8.473,9.051,10.961],
             borderColor:'blue',
             fill: false,
         }]
@@ -24,15 +25,15 @@ new Chart('chart1', {
 new Chart('chart2', {
     type: 'line',
     data: {
-        labels: xValues,
+        labels: xValues2,
         datasets: [{
             label: 'Ngannou',
-            data: [0,1.474,2.808,4.703,6.133,8.743,12.516,11.916,10.249,12.712,13.178,14.643,15.491,19.021],
+            data: [0,2.4625,2.929,4.394,5.241,8.772],
             borderColor:'red',
             fill: false,
         }, {
             label: 'Gane',
-            data: [0,1.72,2.736,4.069,6.873,8.473,9.051,10.961,12.527,14.093,15.659,17.225,18.79,20.356],
+            data: [0,1.333,4.138,5.738,6.315,8.223],
             borderColor:'blue',
             fill: false,
         }]
