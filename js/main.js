4d514d2a1b85ef00b04cfdf289dc3d0beb74b863
John Febbo <jfebbo5@gmail.com>
2022-02-03 11:53:50 -0500

    Edit dominance rating

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const xValues1 = [0,'Fight 1','Fight 2','Fight 3','Fight 4','Fight 5','Fight 6','Fight 7','Fight 8','Fight 9','Fight 10','Fight 11','Fight 12','Fight 13'];
-const xValues2 = [0,'Fight 1','Fight 2','Fight 3','Fight 4','Fight 5'];
 
 new Chart('chart1', {
     type: 'line',
@@ -7,33 +6,12 @@ new Chart('chart1', {
         labels: xValues1,
         datasets: [{
             label: 'Ngannou',
-            data: [0,1.474,2.808,4.703,6.133,8.743,13.459,12.859,11.193,13.656,14.122,15.586,16.434,19.965],
+            data: [0,0.660,1.256,2.103,2.940,4.282,6.950,6.101,5.708,7.678,8.565,10.518,12.013,15.543],
             borderColor:'red',
             fill: false,
         }, {
             label: 'Gane',
-            data: [0,1.72,2.736,4.069,6.873,8.473,9.051,10.961],
-            borderColor:'blue',
-            fill: false,
-        }]
-    },
-    options: {
-        legend: {display: true}
-    }
-});
-
-new Chart('chart2', {
-    type: 'line',
-    data: {
-        labels: xValues2,
-        datasets: [{
-            label: 'Ngannou',
-            data: [0,2.4625,2.929,4.394,5.241,8.772],
-            borderColor:'red',
-            fill: false,
-        }, {
-            label: 'Gane',
-            data: [0,1.333,4.138,5.738,6.315,8.223],
+            data: [0,0.769,1.223,1.820,2.878,3.772,4.479,6.134],
             borderColor:'blue',
             fill: false,
         }]
